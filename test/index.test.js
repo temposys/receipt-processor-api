03c34912d1fb2d29cc7293d0f308ec65f60ec1f8
iana.sergieieva <iana.sergieieva@bahnhof.net>
2025-02-09 20:57:43 -0800

    test coverage of /receipts/:id/points

--- test/index.test.js
+++ test/index.test.js
@@ -109,10 +109,12 @@ describe("POST /receipts/process", () => {
 });
 
 describe("GET /receipts/:id/points", () => {
-    let receiptId;
+    let receiptId1;
+    let receiptId2;
 
     beforeAll(async () => {
-        const res = await request(server)
+        // case 1
+        let res = await request(server)
             .post("/receipts/process")
             .send({
                 retailer: "Test Store",
@@ -125,19 +127,42 @@ describe("GET /receipts/:id/points", () => {
                     { shortDescription: "Item31", price: "15.00" },
                 ],
             });
-        receiptId = res.body.id; // Save Receipt ID which we will test
+        receiptId1 = res.body.id; // Save Receipt ID which we will test
+        // case 2
+        res = await request(server)
+            .post("/receipts/process")
+            .send({
+                retailer: "& -",
+                purchaseDate: "2025-01-02",
+                purchaseTime: "13:03",
+                total: "35.30",
+                items: [
+                    { shortDescription: "Item1", price: "10.00" },
+                    { shortDescription: "Item2", price: "10.00" },
+                    { shortDescription: "Item3", price: "15.30" },
+                ],
+            });
+        receiptId2 = res.body.id; // Save Receipt ID which we will test
     });
 
-    it("should return points for a valid receipt", async () => {
-        const res = await request(server).get(`/receipts/${receiptId}/points`);
+    it("should return points for a valid receipt (case 1)", async () => {
+        const res = await request(server).get(`/receipts/${receiptId1}/points`);
 
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("points");
         expect(res.body.points).toBe(108);
     });
 
+    it("should return points for a valid receipt (case 2)", async () => {
+        const res = await request(server).get(`/receipts/${receiptId2}/points`);
+
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("points");
+        expect(res.body.points).toBe(5);
+    });
+
     it("should return 404 if receipt not found", async () => {
-        const invalidId = "non-existent-id";
+        const invalidId = "834ce6ea-6e14-4738-b97e-18965bcfadd4";
         const res = await request(server).get(`/receipts/${invalidId}/points`);
 
         expect(res.status).toBe(404);
